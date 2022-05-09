366aae4b72941d774e138f5af146211380ab34d4
Pascal Ellenberger <pascal.ellenberger@protonmail.com>
2022-05-10 00:13:53 +0200

    add pagination

--- src/core.clj
+++ src/core.clj
@@ -2,10 +2,15 @@
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]))
 
+(defn make-step [access-token]
+  (fn [next-token]
+    (let [params {"query" "restaurant" "key" access-token "pageToken" next-token}
+          places-url "https://maps.googleapis.com/maps/api/place/textsearch/json"]
+      (->
+        (client/get places-url {:accept :json :query-params params})
+        :body
+        (json/read-str :key-fn keyword)))))
+
 (defn -main [& args]
-  (let [params {"query" "restaurant" "key" (first args)}
-        places-url "https://maps.googleapis.com/maps/api/place/textsearch/json"]
-    (prn (->
-           (client/get places-url {:accept :json :query-params params})
-           :body
-           (json/read-str :key-fn keyword)))))
+  (let [access-token (first args)]
+    (prn (take 21 (flatten (lazy-seq (iteration (make-step access-token) {:vf :results :kf :next_page_token})))))))
